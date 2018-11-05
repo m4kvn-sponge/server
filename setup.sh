f149b0b69a36c0d33d44869cbb2d0a9dc0213785
m4kvn <fun.masahiro@gmail.com>
2018-11-05 16:05:31 +0900

    Create data folder

--- setup.sh
+++ setup.sh
@@ -1,10 +1,22 @@
 #!/bin/sh
 
+DIR=$(cd $(dirname $0) && pwd)
+DATA=$DIR/data
+SPONGE=$DATA/spongeforge.jar
+FORGE_INSTALLER=$DATA/forge-installer.jar
+
 # download spongeforge
-wget -O spongeforge.jar https://repo.spongepowered.org/maven/org/spongepowered/spongeforge/1.12.2-2768-7.1.4/spongeforge-1.12.2-2768-7.1.4.jar
+if [ ! -s $SPONGE ]; then
+    wget -O $SPONGE https://repo.spongepowered.org/maven/org/spongepowered/spongeforge/1.12.2-2768-7.1.4/spongeforge-1.12.2-2768-7.1.4.jar
+fi
 
 # download forge
-wget -O forge-installer.jar "https://files.minecraftforge.net/maven/net/minecraftforge/forge/1.12.2-14.23.5.2768/forge-1.12.2-14.23.5.2768-installer.jar"
+if [ ! -s $FORGE_INSTALLER ]; then
+    wget -O $FORGE_INSTALLER "https://files.minecraftforge.net/maven/net/minecraftforge/forge/1.12.2-14.23.5.2768/forge-1.12.2-14.23.5.2768-installer.jar"
+fi
+
+exit 0
+cd $DIR
 
 # install forge
 java -jar forge-installer.jar --installServer
