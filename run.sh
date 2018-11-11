e3e23e91eb8ce2a7482ef4367e3b94a9b58d93d8
m4kvn <fun.masahiro@gmail.com>
2018-11-11 20:01:23 +0900

    Fix run script

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 CURRENT=$(pwd)
 DIR=$(cd $(dirname $0) && pwd)
-DATA=$DIR/DATA
+DATA=$DIR/data
 FILE=$(ls $DATA | grep "forge-.*-universal.jar")
 
 cd $DATA && java -Xmx3G -Xms1G -d64 -jar $FILE nogui
