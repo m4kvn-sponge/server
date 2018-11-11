5e5146a4ce27267ebceac7f363f28499d54e3b60
m4kvn <fun.masahiro@gmail.com>
2018-11-11 17:00:54 +0900

    Add forge install script

--- setup.sh
+++ setup.sh
@@ -5,18 +5,21 @@ DATA=$DIR/data
 SPONGE=$DATA/spongeforge.jar
 FORGE_INSTALLER=$DATA/forge-installer.jar
 
+SPONGE_VERSION=1.12.2-2768-7.1.4
+FORGE_VERSION=1.12.2-14.23.5.2768
+
+SPONGE_URL="https://repo.spongepowered.org/maven/org/spongepowered/spongeforge/$SPONGE_VERSION/spongeforge-$SPONGE_VERSION.jar"
+FORGE_URL="https://files.minecraftforge.net/maven/net/minecraftforge/forge/$FORGE_VERSION/forge-$FORGE_VERSION-installer.jar"
+
 # download spongeforge
 if [ ! -s $SPONGE ]; then
-    wget -O $SPONGE https://repo.spongepowered.org/maven/org/spongepowered/spongeforge/1.12.2-2768-7.1.4/spongeforge-1.12.2-2768-7.1.4.jar
+    wget -O $SPONGE $SPONGE_URL
 fi
 
 # download forge
 if [ ! -s $FORGE_INSTALLER ]; then
-    wget -O $FORGE_INSTALLER "https://files.minecraftforge.net/maven/net/minecraftforge/forge/1.12.2-14.23.5.2768/forge-1.12.2-14.23.5.2768-installer.jar"
+    wget -O $FORGE_INSTALLER $FORGE_URL
 fi
 
-exit 0
-cd $DIR
-
 # install forge
-java -jar forge-installer.jar --installServer
+cd $DATA && java -jar forge-installer.jar --installServer
